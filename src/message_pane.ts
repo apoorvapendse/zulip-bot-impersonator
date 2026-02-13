eb7934fa272996b82e205f2d1d31c351410ad40c
Steve Howell <showell30@yahoo.com>
2026-02-13 01:31:30 -0500

    Split out message_view_header.

--- src/message_pane.ts
+++ src/message_pane.ts
@@ -1,25 +1,8 @@
 import type { RawMessage, Topic } from "./model";
 
 import { MessageRow } from "./message_row";
+import { MessageViewHeader } from "./message_view_header";
 import * as model from "./model";
-
-function render_topic_heading_count(count: number): HTMLElement {
-    const div = document.createElement("div");
-    div.innerText = `(${count})`;
-    div.style.padding = "3px";
-    div.style.marginLeft = "3px";
-
-    return div;
-}
-
-function render_topic_heading(topic_name: string): HTMLElement {
-    const div = document.createElement("div");
-    div.innerText = topic_name;
-    div.style.color = "#000080";
-    div.style.fontSize = "19px";
-
-    return div;
-}
 
 class MessageList {
     div: HTMLElement;
@@ -48,23 +31,6 @@ class MessageList {
     }
 }
 
-class MessageTopicLine {
-    div: HTMLElement;
-
-    constructor(topic_name: string, topic_count: number) {
-        const div = document.createElement("div");
-        div.style.display = "flex";
-        div.style.borderBottom = "1px solid black";
-        div.style.paddingBottom = "6px";
-        div.style.marginBottom = "12px";
-
-        div.append(render_topic_heading(topic_name));
-        div.append(render_topic_heading_count(topic_count));
-
-        this.div = div;
-    }
-}
-
 export class MessagePane {
     div: HTMLElement;
 
@@ -86,7 +52,7 @@ export class MessagePane {
 
         div.innerHTML = "";
 
-        const topic_line = new MessageTopicLine(topic.name, messages.length);
+        const topic_line = new MessageViewHeader(topic.name, messages.length);
 
         const message_list = new MessageList(messages);
 
---
+++ src/message_view_header.ts
@@ -0,0 +1,35 @@
+function render_topic_heading_count(count: number): HTMLElement {
+    const div = document.createElement("div");
+    div.innerText = `(${count})`;
+    div.style.padding = "3px";
+    div.style.marginLeft = "3px";
+
+    return div;
+}
+
+function render_topic_heading(topic_name: string): HTMLElement {
+    const div = document.createElement("div");
+    div.innerText = topic_name;
+    div.style.color = "#000080";
+    div.style.fontSize = "19px";
+
+    return div;
+}
+
+export class MessageViewHeader {
+    div: HTMLElement;
+
+    constructor(topic_name: string, topic_count: number) {
+        const div = document.createElement("div");
+        div.style.display = "flex";
+        div.style.borderBottom = "1px solid black";
+        div.style.paddingBottom = "6px";
+        div.style.marginBottom = "12px";
+
+        div.append(render_topic_heading(topic_name));
+        div.append(render_topic_heading_count(topic_count));
+
+        this.div = div;
+    }
+}
+
