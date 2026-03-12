7678d2f14ff10ab7f0fb3e5971f74100eb5429b0
Steve Howell <showell30@yahoo.com>
2026-03-12 13:58:43 -0400

    Use slightly fancy strategy for message index.

--- src/backend/message_index.ts
+++ src/backend/message_index.ts
@@ -6,58 +6,59 @@ type Item = {
 };
 
 export class MessageIndex {
-    items: Item[];
+    ids: number[];
 
     constructor(message_map: MessageMap) {
-        this.items = [];
+        this.ids = [];
 
         for (const message of message_map.values()) {
             const message_id = message.id;
             const topic_id = message.topic_id;
-            this.items.push({ message_id, topic_id });
+            this.add_item({ message_id, topic_id });
         }
 
         this.fake_large();
     }
 
-    fake_large(): void {
-        let message_id = 1000;
-        const max_size = 1_000_000;
-        const items = this.items;
-
-        let i = 0;
-        while (items.length < max_size) {
-            message_id += 1;
-            i += 1;
-            const topic_id = items[i].topic_id;
-            items.push({ message_id, topic_id });
-        }
-        console.log("SIZE", this.items.length);
+    add_item(item: Item): void {
+        this.ids.push(item.topic_id);
+        this.ids.push(item.message_id);
     }
 
-    add_to_index(item: Item) {
-        this.items.push(item);
-    }
-
-    find_message_ids_for_topic_id(topic_id: number): Set<number> {
+    find_message_ids_for_topic_id(topic_id: number): number[] {
+        console.log("start find");
         const t = performance.now();
-
-        const items = this.items;
-        const message_ids = items
-            .filter((item) => item.topic_id === topic_id)
-            .map((item) => item.message_id);
-
         let elapsed;
+
+        const ids = this.ids;
+        const size = this.ids.length;
+        const message_ids = [];
+
+        for (let i = 0; i < size; i += 2) {
+            if (ids[i] === topic_id) {
+                message_ids.push(ids[i + 1]);
+            }
+        }
 
         elapsed = performance.now() - t;
         console.log("time to find", elapsed.toFixed(6));
 
-        const message_set = new Set(message_ids);
+        console.log(message_ids);
+        return message_ids;
+    }
 
-        elapsed = performance.now() - t;
-        console.log("time with set:pizza:", elapsed.toFixed(6));
+    fake_large(): void {
+        let message_id = 1000;
+        const max_size = 1_000_000;
+        const ids = this.ids;
 
-        console.log(message_set);
-        return message_set;
+        let i = 0;
+        while (ids.length < max_size * 2) {
+            message_id += 1;
+            const topic_id = ids[i];
+            this.add_item({ message_id, topic_id });
+            i += 2;
+        }
+        console.log("SIZE", this.ids.length);
     }
 }
